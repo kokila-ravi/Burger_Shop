6953ed6bbc50a9d56bd5745aaff3b272192311ba
Kokila R <146109332+kokila-ravi@users.noreply.github.com>
2024-08-19 09:59:46 +0530

    Add files via upload

---
+++ BurgerShop/products.js
@@ -0,0 +1,60 @@
+const products = [
+    {
+        "id": 1,
+        "name":"SPICY BURGER ",
+        "price": 200,
+        "image": "images/1.png",
+        "description": "A spicy burger is a bold and flavorful twist on the classic burger, designed for those who crave a little heat. It features a succulent beef patty seasoned with a blend of spices, topped with spicy jalapeños, pepper jack cheese, and a zesty chipotle sauce. Served on a toasted bun, this burger often includes fresh lettuce, tomatoes, and onions to balance the heat.  "
+    },
+    {
+        "id": 2,
+        "name":"CHICKEN BURGER",
+        "price": 250,
+        "image": "images/2.png",
+        "description": "A chicken burger is a delicious and lighter alternative to the traditional beef burger, featuring a tender, juicy chicken patty as the star. Grilled or crispy, the patty is seasoned to perfection and paired with fresh lettuce, tomatoes, and a slice of cheese, all nestled in a soft, toasted bun. Often enhanced with a spread of mayo or a tangy sauce, this burger offers a satisfying blend of flavors and textures that make it a go-to option for chicken lovers. "
+    },
+    {
+        "id": 3,
+        "name":"PATTY MELT",
+        "price": 290,
+        "image": "images/3.png",
+        "description": "A patty burger is a straightforward yet satisfying classic that focuses on the essentials: a juicy, well-seasoned beef patty as the centerpiece. Grilled to perfection, this burger is served on a soft, toasted bun and can be customized with your choice of toppings like lettuce, tomatoes, onions, pickles, and cheese. Simple yet flavorful, the patty burger is all about savoring the rich, meaty goodness of the patty itself, making it a timeless favorite for burger enthusiasts who appreciate quality and taste in every bite. "
+    },
+    {
+        "id": 4,
+        "name":"CHEESY BURGER",
+        "price": 200,
+        "image": "images/4.webp",
+        "description": "A cheesy burger is all about indulgence and flavor, featuring a juicy beef patty smothered in your choice of melted cheese. Whether you prefer classic American, sharp cheddar, gooey Swiss, or creamy blue cheese, this burger delivers a rich, savory experience. Typically served on a toasted bun with optional toppings like lettuce, tomatoes, pickles, and onions, it’s often complemented by a spread of ketchup or mustard.  "
+    },
+    {
+        "id": 5,
+        "name":"BBQ BURGER",
+        "price": 300,
+        "image": "images/5.png",
+        "description": "A BBQ burger is a smoky, flavor-packed creation that brings the taste of a summer cookout to your plate. It features a juicy beef patty, grilled to perfection, and generously slathered with tangy BBQ sauce. Often topped with crispy onion rings, melted cheddar cheese, and sometimes even a slice of bacon, this burger delivers a rich, savory experience with every bite. Served on a toasted bun, the BBQ burger is a perfect blend of sweet, smoky, and savory flavors, making it a favorite for those who love a bold and hearty meal."
+    },
+    {
+        "id": 6,
+        "name":"VEG BURGER",
+        "price": 200,
+        "image": "images/6.png",
+        "description": "A veg burger is a delicious and wholesome alternative to meat-based burgers, perfect for vegetarians and those looking for a lighter option. The patty is typically made from a blend of vegetables, legumes, grains, and spices, offering a satisfying texture and rich flavor. Common ingredients include chickpeas, black beans, lentils, or even hearty vegetables like mushrooms and sweet potatoes. Served on a toasted bun, it’s often topped with fresh lettuce, tomatoes, onions, and a slice of cheese or vegan cheese, along with your favorite condiments or a zesty sauce."
+    },
+    {
+        "id": 7,
+        "name":" GRILL BURGER",
+        "price": 200,
+        "image": "images/7.png",
+        "description": "A grill burger is a classic favorite that emphasizes the smoky, charred flavor achieved through grilling. The burger features a juicy beef patty, grilled to perfection, with a slight crispiness on the outside while staying tender and flavorful inside. Served on a toasted bun, it’s often topped with fresh lettuce, ripe tomatoes, onions, and a slice of melted cheese, along with your choice of condiments like ketchup, mustard, or BBQ sauce."
+    },
+    {
+        "id": 8,
+        "name":"CHEESE BURGER",
+        "price": 200,
+        "image": "images/8.png",
+        "description": "A cheeseburger is a mouthwatering classic that features a juicy beef patty, perfectly grilled and topped with a slice of melted cheese.Each bite offers a satisfying combination of savory flavors and textures that make it an irresistible choice for burger lovers. "
+    }
+
+];
+export default products;
